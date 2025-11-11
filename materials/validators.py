1ebd265138b169d53d0196224f6440a4af949db5
SergeySN84 <Sergsvircuk@mail.ru>
2025-11-11 22:01:32 +0700

    Создание модулей paginators.py и validators.py в проекте.

---
+++ materials/validators.py
@@ -0,0 +1,15 @@
+import re
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+def validate_video_url(value):
+    """
+    Проверяет, что ссылка ведёт только на youtube.com или youtu.be
+    """
+    if value:
+        youtube_regex = r'^(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/).+$'
+        if not re.match(youtube_regex, value):
+            raise ValidationError(
+                _('Ссылка на видео должна вести только на YouTube.'),
+                code='invalid_video_url'
+            )
