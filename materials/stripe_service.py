f2600be19bca91c0d03c672c53891e5a1304adfa
SergeySN84 <Sergsvircuk@mail.ru>
2025-11-18 18:32:18 +0700

    Создание модулей в приложении payments, внесение изменений в .env.example

---
+++ materials/stripe_service.py
@@ -0,0 +1,36 @@
+import stripe
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def create_stripe_product(name: str, description: str = ""):
+    """Создаёт продукт в Stripe"""
+    return stripe.Product.create(name=name, description=description)
+
+
+def create_stripe_price(product_id: str, amount: int, currency: str = "usd"):
+    """
+    Создаёт цену в Stripe.
+    """
+    return stripe.Price.create(
+        product=product_id,
+        unit_amount=amount,
+        currency=currency,
+        recurring=None  # one-time payment
+    )
+
+
+def create_stripe_session(price_id: str, success_url: str, cancel_url: str):
+    """Создаёт сессию оплаты"""
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': price_id,
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
+    return session
