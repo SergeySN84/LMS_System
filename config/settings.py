a40cf3e2cdaa21e6eeff4dfd8907183bc2f05478
SergeySN84 <Sergsvircuk@mail.ru>
2026-01-04 16:54:13 +0700

    Внесение изменений в проект

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["84.252.141.96"]
 
 
 # Application definition
